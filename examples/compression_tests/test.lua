87d1c1cdcff54f2495d09506b73246ca3f01f1a8
bieno12 <zeyadshahin2015@gmail.com>
2024-04-12 02:17:11 +0200

    added gzip decompress library

---
+++ examples/compression_tests/test.lua
@@ -0,0 +1,45 @@
+local DEFLATE = require '/mcc/lib/compress/deflatelua'
+
+-- Function to handle errors
+local function handleError(errMsg)
+	if type(errMsg) == "table" then
+		errMsg = textutils.serialise(errMsg)
+	end
+    error(errMsg)
+end
+
+-- Function to uncompress gzip file
+local function uncompressGzip(inputFilename, outputFilename)
+    local fh, err = fs.open(shell.resolve(inputFilename), 'rb')
+    if not fh then
+        handleError(err)
+    end
+    
+    local ofh, err = fs.open(shell.resolve(outputFilename), 'wb')
+    if not ofh then
+        handleError(err)
+    end
+    
+    local success, err = DEFLATE.gunzip {input=fh, output=ofh, disable_crc = true}
+    fh.close()
+    ofh.close()
+    
+    if not success then
+        handleError(err)
+    end
+    
+    print("Uncompression completed. Output saved to", outputFilename)
+end
+
+-- Main function
+local function main()
+    local inputFilename = 'foo.txt.gz' -- Adjust as needed
+    local outputFilename = 'foo.txt' -- Adjust as needed
+
+    local success, err = pcall(uncompressGzip, inputFilename, outputFilename)
+    if not success then
+        handleError(err)
+    end
+end
+
+main()
