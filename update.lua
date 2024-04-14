c5efed9d2fe25b6a582271760a0feb4e89bd4447
bieno12 <zeyadshahin2015@gmail.com>
2024-04-14 07:03:54 +0200

    read settings to get github path

--- update.lua
+++ update.lua
@@ -1,9 +1,10 @@
-local URL_PREFIX = 'https://raw.githubusercontent.com/Black-Hack/mcc/master/'
+local URL_PREFIX = 'https://raw.githubusercontent.com/' .. settings.get("mcc_repo_path",'Black-Hack/mcc/master/')
 local DIR_PREFIX = '/mcc/'
 
 local headers = {}
 headers["cache-control"] = "no-cache"
 headers["pragma"] = "no-cache"
+
 local function entry(name)
     return function()
         local response = http.get(URL_PREFIX .. name, headers, true)
@@ -13,36 +14,10 @@ local function entry(name)
     end
 end
 
-fs.delete(DIR_PREFIX)
 fs.makeDir(DIR_PREFIX)
 fs.makeDir(DIR_PREFIX .. 'lib/')
 
+print(URL_PREFIX)
 parallel.waitForAll(
-    entry('update.lua'),
-    entry('sortchest.lua'),
-    entry('excavate.lua'),
-    entry('lib/sortchest.lua'),
-    entry('LICENSE.md'),
-
-    entry('lib/traverse.lua'),
-
-    entry('inventory_system/controller.lua'),
-    entry('inventory_system/model.lua'),
-    entry('inventory_system/view.lua'),
-
-    entry('lib/advanced_inventory/init.lua'),
-
-    entry('lib/itp/itpserver.lua'),
-    entry('lib/itp/itpclient.lua'),
-
-    entry('gitblob/client.lua'),
-    entry('gitblob/server.lua'),
-    entry('gitblob/gitblob.lua'),
-
-    entry('examples/advanced_inventory/inventory_test.lua'),
-    entry('examples/advanced_inventory/inventory_test.lua'),
-    entry('examples/itp/itpclient_test.lua'),
-    entry('examples/itp/itpserver_test.lua'),
-
-    entry('lava_farmer/lava_farmer.lua')
+    entry('excavate.lua')
 )
