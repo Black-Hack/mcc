f4a24e9f9e1af8c8e631f637c6104528d786d4fe
bieno12 <zeyadshahin2015@gmail.com>
2024-04-16 22:51:35 +0200

    fixed 2table funciton

--- lib/nbt/init.lua
+++ lib/nbt/init.lua
@@ -5,8 +5,23 @@ local modulePath = debug.getinfo(1, "S").source:sub(2)
 local nbtreader = require (modulePath:gsub("[^/]+$", "") .. "nbtreader")
 local nbtwriter = require (modulePath:gsub("[^/]+$", "") .. "nbtwriter")
 
-local nbt = {}                                      -- Define a table to hold our module functions and data
+DEBUG = true
 
+local nbt = {}
+local logger
+local function log(message)
+	if DEBUG then
+		if logger == nil then
+			if fs.exists(shell.resolve "log") then
+				logger = assert(io.open(shell.resolve "log", "a"))
+			else
+				logger = assert(io.open(shell.resolve "log", "w+"))
+			end
+		end
+		logger:write(message .."\n")
+		logger:flush()
+	end
+end
 
 -- Function to read an NBT file and convert it to a Lua table
 function nbt.read(filename)
@@ -58,21 +73,23 @@ end
 
 
 function nbt.nbt2table(tag)
-    
-    if tag.type == TAG_COMPOUND then
+    if tag.tagType == TAG_COMPOUND then
         local c = {}
-        for name, innerTag in pairs(tag.content) do
-            c[name] = nbt.nbt2table(innerTag)
-        end
+
+		for name, innerTag in pairs(tag.content) do
+           innerTag = nbt.nbt2table(innerTag)
+			c[name] = innerTag
+		end
         return c
-    elseif tag.type == TAG_LIST then
-        local l = {}
-        for innerTag in pairs(tag.content) do
+    elseif tag.tagType == TAG_LIST then
+			local l = {}
+        for _,innerTag in pairs(tag.content) do
+
             table.insert(l, (nbt.nbt2table(innerTag)))
         end
         return l
     else
        return tag.content
-    end
+    end 
 end
 return nbt -- Return the module table
